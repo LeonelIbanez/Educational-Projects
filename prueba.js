d66d0a5728e67925fdc63ec7d45441fb4f3b0010
LILeonelIbañez <leo.ibanez84@gmail.com>
2024-04-24 11:48:20 -0300

    borrelaprueba

--- prueba.js
+++
@@ -1,8 +0,0 @@
-if (contadorPuntosJug1 > contadorPuntosJug2) {
-    resultado.ganador = jugadorUno.nombre
-  } else if (contadorPuntosJug1 < contadorPuntosJug2) {
-    resultado.ganador = jugadorDos.nombre
-  } else {
-    resultado.ganador = "Empate"
-  }
-  console.log(resultado);
